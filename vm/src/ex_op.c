8d8c4c5db69a4f43149823e18bcd0f3550455af7
Thomas GUILLEMARD <tguillem@student.42.fr>
2017-01-24 16:53:08 +0100

    Fix possible segfault of instruction aff

--- vm/src/ex_op.c
+++ vm/src/ex_op.c
@@ -332,7 +332,6 @@ void op_aff(t_vm *vm, t_process *process, t_op_data *data)
 	int32_t	*input;
 
 	(void)vm;
-	info("[%d]: aff 0x%x\n", process->pid, data->params[0]);
-	input = get_register(process, data, 0);
-	ft_putchar((char)(*input % 256));
+	if (input = get_register(process, data, 0))
+		info("[%d]: aff %c\n", process->pid, (char)(*input % 256));
 }
